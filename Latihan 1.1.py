bdce0a6c940e0be106644db86f17d0e4b9968cd7
wahyualdi-droid <wahyualdi595@gmail.com>
2026-03-19 11:24:48 +0700

    main.py

---
+++ Latihan 1.1.py
@@ -0,0 +1,31 @@
+#Program latihan 1.1: Tipe data dan variabel 
+
+#nama file: file latihan 1.1 . Tipe data dan variabel.py
+
+#mengisi variabel dengan berbagai tipe data 
+
+#tipe data string (kata/kalimat)
+nama = "wahyu aldi"
+
+#tipe data integer (bilangan bulat)
+usia = 19
+#tipe data float (desimal)
+tiggi_badan = 165,6
+alamat ="tual, maluku"
+#tipe data string 
+pekerjaan = "Mahasiswa"
+# tipe data string 
+hobi = "game"
+ # tipe data string 
+is_active = True
+
+#menampilkan data beserta tipe datanya 
+print("---latihan 1.1: output Data & Tipe Data")
+print("nama    : ",nama,"|Tipe:",type(nama))
+print("usia   : ",usia,"Tahun |Tipe:",type(usia))
+print("Tinggi     : ",tiggi_badan," cm |Tipe:",type(tiggi_badan))
+print("Alamat    : ",alamat,"|Tipe:",type(alamat))
+print("pekerjaan   : ",pekerjaan,"|Tipe:",type(pekerjaan))
+print("Hobi  : ",hobi,"|Tipe:",type(hobi))
+print("status    : ",is_active,"|Tipe:",type(is_active))
+
